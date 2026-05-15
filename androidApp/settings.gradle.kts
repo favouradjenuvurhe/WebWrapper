bf19ecc9aa600c8a66505c60640a31649ce10ebd
Favour Adjenuvurhe <133374851+favouradjenuvurhe@users.noreply.github.com>
2026-05-15 08:11:34 +0100

    Create settings.gradle.kts

---
+++ androidApp/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WebWrapper"
+
+include(":app")
